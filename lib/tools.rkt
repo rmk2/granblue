37a4f04b0b76792e923d63184cfd2b4cc14654fb
Ryko <ryko@rmk2.org>
2017-08-01 17:44:16 +0200

    racket: modified > lib/tools.rkt
    removed: initial dummy data entries

--- lib/tools.rkt
+++ lib/tools.rkt
@@ -17,11 +17,6 @@
 (define (dev-setup)
   (dev-cleanup)
   (sql-granblue-create-accountdb)
-  ;; (sql-granblue-update-accountdb
-  ;;  (append (map (lambda (x) (granblue-account null (format "granblue+~a@rmk2.org" x) 0 0 1)) (range 1 11))
-  ;; 	   (map (lambda (x) (granblue-account null (format "ryko+gb~a@rmk2.org" x) 0 0 1)) (range 1 20))
-  ;; 	   (list (granblue-account null "granblue+1@rmk2.org" 0 1 1))
-  ;; 	   (list (granblue-account null "granblue+2@rmk2.org" 0 1 1))))
   (sql-granblue-init-db)
   (sql-granblue-create-characterdb)
   (sql-granblue-create-characterdb-view)
